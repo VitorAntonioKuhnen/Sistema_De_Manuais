900d6e24108326c051efe826b7e4e887d8f21319
Vitor Kuhnen TI <vitor.kuhnen@lidera.corp>
2022-10-24 13:25:44 -0300

    Testes para adição de opção de Busca do campo

--- editorSummernote/models.py
+++ editorSummernote/models.py
@@ -4,5 +4,9 @@ class Manuais(models.Model):
     titulo = models.CharField(max_length=255)
     conteudo = models.TextField()
 
+
+    class Meta:
+        db_table = 'Manuais'
+
     def __str__(self):
         return self.titulo
--- editorSummernote/views.py
+++ editorSummernote/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 from .models import Manuais
 def principal(request):
     manuais = Manuais.objects.all()
+
+    busca = request.GET.get('search')
+    if busca:
+        print(busca)
+        manuais = Manuais.objects.filter(conteudo = busca)
+        print(manuais)
     return render(request, 'manuais.html', {'manuais':manuais})
