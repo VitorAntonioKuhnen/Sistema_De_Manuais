1b793e5318e152fb3015df1fd83265c035c65427
VitorAntonioKuhnen <vitorkuhnen14@gmail.com>
2022-10-27 21:57:14 -0300

    Testando filtro por Titulo

--- editorSummernote/views.py
+++ editorSummernote/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from .models import Manuais
+from django.db.models import Q
+
 def principal(request):
     manuais = Manuais.objects.all()
 
     busca = request.GET.get('search')
     if busca:
         print(busca)
-        manuais = Manuais.objects.filter(conteudo__icontains = busca) #O termo icontains serve para realizarmos uma busca com o que foi digitado
+        # manuais = Manuais.objects.filter(conteudo__icontains = busca) #O termo icontains serve para realizarmos uma busca com o que foi digitado
+        manuais = Manuais.objects.filter(Q(titulo=busca, conteudo=busca)) #Verificar como utilizar o Q()
         print(manuais)
     return render(request, 'manuais.html', {'manuais':manuais})
 
