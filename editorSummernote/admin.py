26a7fcf1479fe302eb7fc3bd101ff2fc14992247
VitorAntonioKuhnen <vitorkuhnen14@gmail.com>
2022-10-31 13:15:35 -0300

    Adicionado filtro pela TAG

--- editorSummernote/admin.py
+++ editorSummernote/admin.py
@@ -4,9 +4,9 @@ from django_summernote.admin import SummernoteModelAdmin
 
 
 class ManuaisAdmin(SummernoteModelAdmin):
-    list_display = ['titulo', 'conteudo']
-    list_display_links = ['titulo', 'conteudo']
-    search_fields = ['titulo', 'conteudo']
-    summernote_fields = ('conteudo',)
+    list_display = ['titulo']
+    list_display_links = ['titulo']
+    search_fields = ['titulo', 'conteudo', 'tag']
+    summernote_fields = ('conteudo')
 
 admin.site.register(Manuais, ManuaisAdmin)    
--- editorSummernote/views.py
+++ editorSummernote/views.py
@@ -9,7 +9,7 @@ def principal(request):
     if busca:
         print(busca)
         # manuais = Manuais.objects.filter(conteudo__icontains = busca) #O termo icontains serve para realizarmos uma busca com o que foi digitado
-        manuais = Manuais.objects.filter(Q(titulo__icontains=busca) | Q(conteudo__icontains=busca)) #Verificar como utilizar o Q() 
+        manuais = Manuais.objects.filter(Q(titulo__icontains=busca) | Q(conteudo__icontains=busca) | Q(tag__icontains=busca)) #Verificar como utilizar o Q() 
         print(manuais)    
     return render(request, 'manuais.html', {'manuais':manuais})
 
